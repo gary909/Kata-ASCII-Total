590150052ddf173f71079d4a3af015dc6c720a1d
gary909 <garywhite61@hotmail.com>
2023-02-06 12:42:36 +0100

    code

---
+++ app.js
@@ -0,0 +1,12 @@
+function uniTotal (string) {
+    // total up dem unicodes!
+}
+
+console.log(uniTotal("")); // 0
+console.log(uniTotal("a")); // 97
+console.log(uniTotal("b")); // 98
+console.log(uniTotal("c")); // 99
+console.log(uniTotal("d")); // 100
+console.log(uniTotal("e")); // 101
+console.log(uniTotal("aaa")); // 291
+console.log(uniTotal("Mary Had A Little Lamb")); // 1873
